875af58db3e443efb9e931f01fee754ff02f8a82
Jorge Andrés Facundo Solarte <50686761+facundo1220@users.noreply.github.com>
2023-03-08 10:32:05 -0500

    Delete FrontEnd directory

--- FrontEnd/src/components/Body.jsx
+++
@@ -1,9 +0,0 @@
-function body({ content }) {
-  return (
-    <div className="min-h-screen bg-slate-5s0 static p-2 pt-20 bg-[#111827]">
-      {content}
-    </div>
-  );
-}
-
-export default body;
--- FrontEnd/src/components/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import Logo from "../images/Logo.png";
-
-function logo() {
-  return <img className="h-9" src={Logo} alt="des" />;
-}
-
-export default logo;
